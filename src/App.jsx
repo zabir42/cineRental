6830bf2817426a62dae93cbdc29a3da574c6e979
zabir42 <xabir1079@gmail.com>
2024-01-25 03:27:38 +0600

    something bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer, useState } from "react";
 import Page from "./Page";
 import { MovieContext, ThemeContext } from "./context";
-import { cartReducer, initialState } from "./reducers/CartReducer";
+import { cartReducer, initialState } from "./reducers/cartReducer";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(true);
